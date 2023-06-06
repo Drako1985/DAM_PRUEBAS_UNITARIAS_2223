ca64a1bccdd590bdf645052f11d26b48cf99b62d
fer.alb@gmail.com <alumno@AULA2521>
2023-06-06 20:26:51 +0200

    a medias, sin tiempo

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4d84d7e-2f76-445d-83ff-9f9dd41e6f98")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/gestionBancaria.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using gestionBancariaApp;
+using System.Security.Cryptography.X509Certificates;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class gestionBancaria
+    {
+        [TestMethod]
+        public void aperturaCuenta()
+        {
+            double saldoApertura = 1;//damos valor inicial
+            double saldoLimite = 100;//el limite que queremos
+            double saldoIntermedio = 10;//valor intermedio
+
+            gestionBancaria prueba = new gestionBancaria();
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
